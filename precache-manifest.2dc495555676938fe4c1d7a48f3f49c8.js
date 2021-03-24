41f54e47d6e7a441a50fabda0a171f1db22162af
Stylesproline <stylesproline@gmail.com>
2021-03-24 14:11:37 +0200

    Updates

---
+++ precache-manifest.2dc495555676938fe4c1d7a48f3f49c8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae41e4f9ed3b74e48a9052efde1214c2",
+    "url": "/openland/index.html"
+  },
+  {
+    "revision": "165e421be7dd429c87f3",
+    "url": "/openland/static/css/main.ff873095.chunk.css"
+  },
+  {
+    "revision": "cb170ed7f435afc85587",
+    "url": "/openland/static/js/2.27505fc1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/openland/static/js/2.27505fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165e421be7dd429c87f3",
+    "url": "/openland/static/js/main.3fd71e17.chunk.js"
+  },
+  {
+    "revision": "42aae5dcafb4cd227313",
+    "url": "/openland/static/js/runtime-main.a592a6f1.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/openland/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/openland/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/openland/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/openland/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/openland/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/openland/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/openland/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "5399948c803b15ce20bf7276c600dadf",
+    "url": "/openland/static/media/features-split-image-01.5399948c.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/openland/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "0c4d292ba461ce275396d6d25212a941",
+    "url": "/openland/static/media/features-split-image-03.0c4d292b.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/openland/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/openland/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/openland/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/openland/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
